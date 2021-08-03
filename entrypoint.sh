d5bad66b337bf640db0d0cea8103c08941071326
cstallar <69056769+chris-stallard1@users.noreply.github.com>
2021-08-03 16:31:04 -0400

    why

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,3 +7,4 @@ if [ "$1" == "http" ]; then
     sudo python3 -m hypercorn --bind 0.0.0.0:80 server:app
 else
     sudo python3 -m hypercorn --certfile /https/certfile.pem --keyfile /https/privkey.pem --bind 0.0.0.0:443 --insecure-bind 0.0.0.0:80 server:redirectedApp
+fi
