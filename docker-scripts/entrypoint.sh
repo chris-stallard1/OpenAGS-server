36e0931e4a3654a768389c4d3887f28492f698f1
cstallar <69056769+chris-stallard1@users.noreply.github.com>
2021-08-04 09:52:51 -0400

    Update entrypoint.sh

--- docker-scripts/entrypoint.sh
+++ docker-scripts/entrypoint.sh
@@ -4,7 +4,7 @@ python3 -m pip install -U openags
 
 cd /OpenAGS-server
 if [ "$1" == "http" ]; then
-    python3 -m hypercorn --bind 0.0.0.0:8080 server:app
+    python3 -m hypercorn --bind 0.0.0.0:80 server:app
 else
     echo "$2" > hostname
     python3 -m hypercorn --certfile /https-certs/certfile.pem --keyfile /https-certs/privkey.pem --bind 0.0.0.0:443 --insecure-bind 0.0.0.0:80 server:redirectedApp
