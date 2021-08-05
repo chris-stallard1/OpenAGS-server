0fc0b5d711bb0aa628c7de9a8e55bb622fc08eb8
cstallar <69056769+chris-stallard1@users.noreply.github.com>
2021-08-05 12:30:49 -0400

    fix service worker

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-python3 -m pip install -U openags
 cd /home/openags/OpenAGS-server
-python3 -m hypercorn --bind 0.0.0.0:8080 server:app > ../server.log 2>&1 &
+python3 -m hypercorn --bind 0.0.0.0:8080 server:app > ../server.log 2>&1
