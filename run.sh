d0e4b13cdd1aaadf15e17459f0b8cbaea65d0301
cstallar <69056769+chris-stallard1@users.noreply.github.com>
2021-08-05 11:09:41 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python3 -m pip install -U openags
 cd /home/openags/OpenAGS-server
-python3 -m hypercorn --bind 0.0.0.0:8080 server:app > ../server.log 2>&1 &
+sudo python3 -m hypercorn --bind 0.0.0.0:8080 server:app > ../server.log 2>&1 &
